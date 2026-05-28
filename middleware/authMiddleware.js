4403e63499b5d2b7f48e8f3ea7e272ada3480a6f
Fenil Nagar <fenilnagar001@gmail.com>
2026-05-28 12:28:33 +0530

    handle deleted user with valid message in auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -15,6 +15,11 @@ const protect = async (req, res, next) => {
 
       req.user = await User.findById(decoded.id).select('-password');
 
+      if (!req.user) {
+  return res.status(401).json({ message: 'User no longer exists' });
+}
+
+
       next(); 
     } catch (error) {
       res.status(401).json({ message: 'Not authorized, token failed' });
